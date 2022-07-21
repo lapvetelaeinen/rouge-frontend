956d1918b94afe0c9ec90d7d908f97be809e0901
Rouge <filip.lapvetelainen@icloud.com>
2022-07-21 10:30:52 +0200

    added modal

--- pages/event/[id].js
+++ pages/event/[id].js
@@ -4,10 +4,12 @@ import { useContext, useEffect, useState } from "react";
 import Image from "next/image";
 import useSWR from "swr";
 import axios from "axios";
+import Times from "../../components/svg/Times";
 
 export default function EventPage({ data }) {
   const router = useRouter();
   const passedID = router.query.id;
+  const [showModal, setShowModal] = useState(false);
 
   const thisEvent = data.find((event) => event.eventId == passedID);
 
@@ -15,7 +17,26 @@ export default function EventPage({ data }) {
   const imagePath = BUCKET_URL + thisEvent.image;
 
   return (
-    <div className="min-h-screen bg-neutral-800">
+    <div className="min-h-screen bg-neutral-800 relative">
+      <div className="">
+        {showModal ? (
+          <div className="bg-neutral-800 absolute z-50 h-full w-full flex justify-center items-start bg-opacity-80">
+            <div className="bg-neutral-200 w-full min-h-[700px] m-4 rounded-3xl">
+              <div className="flex flex-col">
+                <div className="flex justify-between p-1">
+                  <p></p>
+                  <Times
+                    width={50}
+                    height={50}
+                    fill="#f57971"
+                    onClick={() => setShowModal(!showModal)}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : null}
+      </div>
       <div className="relative">
         <Image
           src={imagePath}
@@ -39,7 +60,10 @@ export default function EventPage({ data }) {
         <p className="pl-2 pt-2 text-2xl text-neutral-300">
           {thisEvent.description}
         </p>
-        <button className="bg-gradient-to-r from-[#d57187] to-violet-400 w-full fixed bottom-0 h-20 text-3xl font-steelfish text-neutral-800 ">
+        <button
+          className="bg-gradient-to-r from-[#d57187] to-violet-400 w-full fixed bottom-0 h-20 text-3xl font-steelfish text-neutral-800"
+          onClick={() => setShowModal(!showModal)}
+        >
           KÖP BILJETT FRÅN 199 SEK
         </button>
       </div>
