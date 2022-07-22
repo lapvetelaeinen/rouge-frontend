b44616d375292ba1b33a5b15d43c90586078e69e
Rouge <filip.lapvetelainen@icloud.com>
2022-07-22 14:24:24 +0200

    updated ticket modal

--- pages/event/[id].js
+++ pages/event/[id].js
@@ -11,10 +11,20 @@ export default function EventPage({ data }) {
   const passedID = router.query.id;
   const [showModal, setShowModal] = useState(false);
 
-  const [value, setValue] = useState("fruit");
+  const [value, setValue] = useState("");
+  const [price, setPrice] = useState("");
 
   const handleChange = (event) => {
     setValue(event.target.value);
+    const chosenTicket = thisEvent.tickets.find(
+      (ticket) => ticket.class == event.target.value
+    );
+    setPrice(chosenTicket.price);
+  };
+
+  const openModal = () => {
+    setPrice(thisEvent.tickets[0].price);
+    setShowModal(!showModal);
   };
 
   const thisEvent = data.find((event) => event.eventId == passedID);
@@ -40,18 +50,60 @@ export default function EventPage({ data }) {
                     onClick={() => setShowModal(!showModal)}
                   />
                 </div>
-                <div>
-                  <label>
-                    Biljettklass
-                    <select value={value} onChange={handleChange}>
+                <div className="mx-5 flex items-center mb-8">
+                  <Image
+                    src={imagePath}
+                    width={100}
+                    height={100}
+                    className="rounded-md"
+                    alt=""
+                  />
+                  <div className="pl-5 text-neutral-700">
+                    <p className="text-2xl">{thisEvent.title}</p>
+                    <p>{thisEvent.date.substring(0, 10)}</p>
+                  </div>
+                </div>
+                <div className="bg-neutral-300 mx-5 rounded-lg p-4 mb-4 shadow-md">
+                  <div className="flex text-xl ">
+                    <p className="text-neutral-700">Biljettklass:</p>
+                    <select
+                      value={value}
+                      onChange={handleChange}
+                      className="ml-4 bg-neutral-200 rounded-md text-neutral-500 shadow-sm"
+                    >
                       {thisEvent.tickets.map((ticket) => (
                         <option key={ticket.class} value={ticket.class}>
                           {ticket.class}
                         </option>
                       ))}
                     </select>
-                  </label>
+                  </div>
+                  <div className=" flex pt-4 text-xl text-neutral-700">
+                    <p>Pris:</p>
+                    <p className="pl-2">{price} SEK</p>
+                  </div>
                 </div>
+                <div className="bg-neutral-300 mx-5 rounded-lg p-4 mb-4 shadow-md">
+                  <div className="flex text-xl text-neutral-700">
+                    <p>Viktig info!</p>
+                  </div>
+                  <div className=" flex pt-4 text-xl">
+                    <p className="text-base text-neutral-500">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Proin sit amet augue vehicula, malesuada enim et, vehicula
+                      enim. Morbi quis faucibus orci.
+                    </p>
+                  </div>
+                </div>
+                <div className="mx-5">
+                  <button className="bg-gradient-to-r from-[#d57187] to-violet-400 w-full  text-4xl font-steelfish text-neutral-800 rounded-lg py-6 shadow-lg">
+                    KÖP
+                  </button>
+                </div>
+                <p className="text-xs text-center pt-4 text-neutral-500">
+                  Genom att klicka på köp så godkänner du våra villkor för
+                  biljettköp.
+                </p>
               </div>
             </div>
           </div>
@@ -82,7 +134,7 @@ export default function EventPage({ data }) {
         </p>
         <button
           className="bg-gradient-to-r from-[#d57187] to-violet-400 w-full fixed bottom-0 h-20 text-3xl font-steelfish text-neutral-800"
-          onClick={() => setShowModal(!showModal)}
+          onClick={() => openModal()}
         >
           KÖP BILJETT FRÅN {thisEvent.tickets[0].price} SEK
         </button>
