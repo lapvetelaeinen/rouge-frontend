51ab79991d6203745fa9e63cc377b64ff7573373
Rouge <filip.lapvetelainen@icloud.com>
2022-07-29 15:08:41 +0200

    ticket to db

--- pages/api/create-order.js
+++ pages/api/create-order.js
@@ -1,16 +1,37 @@
 import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
+    const ticketId = uuidv4();
+
     await axios({
       method: "post",
-      url: "https://ljjqu2ki77.execute-api.eu-west-2.amazonaws.com/default/RougeLambdaSES",
+      url: "https://svngddunt0.execute-api.eu-west-2.amazonaws.com/tick/ticket",
       headers: {},
       data: {
-        recipent: req.body.email,
-        mess: req.body.event, // This is the body part
+        ticketId: ticketId,
+        eventId: req.body.eventId,
+        event: req.body.event,
+        ticketClass: req.body.ticketClass,
+        owner: req.body.email,
       },
     });
     res.status(200).send("hello");
   }
+
+  // SEND EMAIL
+  //
+  // if (req.method === "POST") {
+  //   await axios({
+  //     method: "post",
+  //     url: "https://ljjqu2ki77.execute-api.eu-west-2.amazonaws.com/default/RougeLambdaSES",
+  //     headers: {},
+  //     data: {
+  //       recipent: req.body.email,
+  //       mess: req.body.event, // This is the body part
+  //     },
+  //   });
+  //   res.status(200).send("hello");
+  // }
 }
