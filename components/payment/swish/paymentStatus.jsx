17f8251b566f251d5957962fd309e3524f18f856
Rouge <filip.lapvetelainen@icloud.com>
2022-08-10 13:57:19 +0200

    testing payment

--- components/payment/swish/paymentStatus.jsx
+++ components/payment/swish/paymentStatus.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import styles from '../../../styles/payment.module.css'
+import styles from "../../../styles/payment.module.css";
 
 const PaymentStatus = (props) => {
   const [message, setMessage] = React.useState(null);
@@ -11,28 +11,41 @@ const PaymentStatus = (props) => {
 
     if (!paymentId) {
       setMessage("Not found payment instance");
-      setIsLoading(false)
+      setIsLoading(false);
       return;
     }
 
-    fetch('/api/payment/swish', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+    const createOrder = async (params) => {
+      await axios
+        .post("/api/order-confirmation", params)
+        .catch(function (error) {
+          if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          }
+        });
+    };
+
+    fetch("/api/payment/swish", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         type: "getPaymentStatus",
-        paymentId: paymentId
+        paymentId: paymentId,
       }),
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log(">>> Payment status", data)
-        setPaymentMeta(data)
+        console.log(">>> Payment status", data);
+        setPaymentMeta(data);
         switch (data.status) {
           case "CREATED":
             setMessage("Payment is created not paid yet.");
             break;
           case "PAID":
             setMessage("Payment succeeded!");
+            console.log(data);
             break;
           case "CANCELLED":
             setMessage("Your payment cancelled.");
@@ -41,7 +54,9 @@ const PaymentStatus = (props) => {
             setMessage("Your payment declined.");
             break;
           case "ERROR":
-            setMessage("An error occurred, like the payment was blocked or timed out. See list of error codes for all potential error conditions.");
+            setMessage(
+              "An error occurred, like the payment was blocked or timed out. See list of error codes for all potential error conditions."
+            );
             break;
           case "fail":
             setMessage(data.message);
@@ -51,39 +66,47 @@ const PaymentStatus = (props) => {
             break;
         }
         return true;
-      }).catch(error => {
+      })
+      .catch((error) => {
         setMessage(error.message || "Something went wrong.");
         return false;
       });
-      setIsLoading(false)
+    setIsLoading(false);
   }, []);
 
   return (
     <div id="swish-payment-status">
       <div>
-        {
-          isLoading ? <h1 className={styles.paymentStatusMessage}>Loading...</h1> : ''
-        }
+        {isLoading ? (
+          <h1 className={styles.paymentStatusMessage}>Loading...</h1>
+        ) : (
+          ""
+        )}
       </div>
-      {
-        paymentMeta && paymentMeta.id &&
+      {paymentMeta && paymentMeta.id && (
         <div className="flex">
           <ul>
-            <li><b>Message</b>: {paymentMeta.message}</li>
-            <li><b>Amount</b>: {paymentMeta.amount}</li>
-            <li><b>Status</b>: {paymentMeta.status}</li>
+            <li>
+              <b>Message</b>: {paymentMeta.message}
+            </li>
+            <li>
+              <b>Amount</b>: {paymentMeta.amount}
+            </li>
+            <li>
+              <b>Status</b>: {paymentMeta.status}
+            </li>
           </ul>
         </div>
-      }
+      )}
 
       {/* Show any error or success messages */}
-      {(message && !isLoading) &&
+      {message && !isLoading && (
         <div>
           <h1 className={styles.paymentStatusMessage}>Status: {message}</h1>
         </div>
-      }
+      )}
     </div>
   );
-}
+};
 
 export default PaymentStatus;
