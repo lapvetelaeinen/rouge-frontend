83578571b963b44502d9b04986f762e63f8a1afe
Rouge <filip.lapvetelainen@icloud.com>
2022-08-16 12:38:56 +0200

    trying

--- components/payment/swish/paymentStatus.jsx
+++ components/payment/swish/paymentStatus.jsx
@@ -47,7 +47,7 @@ const PaymentStatus = (props) => {
           case "PAID":
             setMessage("Payment succeeded!");
             console.log(data);
-            createOrder();
+            createOrder(data);
             break;
           case "CANCELLED":
             setMessage("Your payment cancelled.");
