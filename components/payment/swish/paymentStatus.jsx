a2988a593aaaa1f25e30ffe793b384723516f5f4
Rouge <filip.lapvetelainen@icloud.com>
2022-08-16 18:52:54 +0200

    oja

--- components/payment/swish/paymentStatus.jsx
+++ components/payment/swish/paymentStatus.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import styles from "../../../styles/payment.module.css";
-import { EventContext } from "../contexts/EventContext";
+import { EventContext } from "../../../contexts/EventContext";
 import { useContext } from "react";
 
 const PaymentStatus = (props) => {
